165d86c31bb491548dfcc5e9ee92fd9eb1b0dc47
lyndaanne <lynda.anne.delagarza@gmail.com>
2020-08-10 15:10:35 -0500

    lecture practice with JS and HTML

---
+++ js/JSwithHTML.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// the following line will show the OK/CANCEL confirm dialog
+var confirmed = confirm('Are you sure you want to do XYZ?');
+console.log(confirmed); // will be either true or false
+
+var suprise = "Hello!";
+alert(suprise);
+
+console.log("Hello from JSwithHTML.js")
+
+alert(suprise);
---
+++ js/JSwithHTMLlec.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// console.log(sentence);
+var suprise = "Boo!";
+alert(suprise);
+
+var confirmed = confirm("Are you understanding today's lesson?");
+console.log(confirmed);
+
+var question2 = ("Where do you want to go on vacation?")
+var answer2 = prompt(question2);
+console.log(answer2);
+console.log(typeof answer2);
+
+
+
+
+console.log(surprise);
+
+// console.log('Today I feel inbetween.')
